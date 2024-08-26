e917368e7b2a93aabe9a22fa29b17c9a76e64f18
Zakaria Lachkar <97468839+LACHKAR99ZAKARIA@users.noreply.github.com>
2024-08-26 19:34:56 +0100

    init

---
+++ V2/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  V2
+//
+//  Created by Zakaria Lachkar on 26/8/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+            Text("V2")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ V2/V2App.swift
@@ -0,0 +1,17 @@
+//
+//  V2App.swift
+//  V2
+//
+//  Created by Zakaria Lachkar on 26/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct V2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
